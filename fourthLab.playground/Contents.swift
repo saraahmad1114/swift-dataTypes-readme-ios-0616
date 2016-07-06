921c226ed31c8651bca79758218365de1e559266
Sara <saraahmad1114@yahoo.com>
2016-07-06 09:57:04 -0400

    int stuff

---
+++ fourthLab.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var ninthPlanet: String
+ninthPlanet = "Pluto"
+
+let thirdPlanet = "Earth"
+
+let fifthPlanet: String = "Jupiter"
+let sixthPlanet: String = "Saturn"
+let seventhPlanet: String = "Uranus"
+
+let numberOfMoonsOfSaturn = 62
+//"There are 62 moons orbiting Saturn"
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
